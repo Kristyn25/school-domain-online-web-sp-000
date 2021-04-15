54b06f7215504814c3da9a1631b2075b9317d484
Kristyn Little <kristynlittle@frontier.com>
2021-04-15 04:20:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,6 @@ attr_reader :name, :roster
   end
 
   def add_student(student_name, grade)
-    # if the roster doesn't contain grade yet
     if roster.include?(grade) do
       roster[grade] = []
     end
